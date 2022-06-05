1e05a54299a161bf96b69231ddf42d80050c1583
TestMan <maext@protonmail.com>
2022-06-05 13:16:04 +0200

    fixed some accessibility issues, deleted one line of code from js file

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function getAdviceObj() {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(xhttp);
       xhttp = JSON.parse(xhttp.responseText);
       displayAdvice(xhttp);
     }
